771f8103254da5cd00b7e977bf0c7cb919462f56
Samuel Bridges <samuel.bridges@kcl.ac.uk>
2016-03-06 16:16:53 +0000

    Started Implementing Audio Quiz

---
+++ app/src/main/java/com/grammar/trocket/grammar/com/grammar/trocket/audioQuiz/Audio_Quiz_Questions_List.java
@@ -0,0 +1,29 @@
+package com.grammar.trocket.grammar.com.grammar.trocket.audioQuiz;
+
+/**
+ * Created by Sam on 06/03/2016.
+ */
+public class Audio_Quiz_Questions_List {
+
+    /**
+     * creates the words for the audio player
+     * @return
+     */
+
+    public String[] createArray() {
+        String[] QuestionsList = new String[48];
+        QuestionsList[0] = "el gato"; //cat
+        QuestionsList[1] = "el perro"; //dog
+        QuestionsList[2] = "el pez"; //fish
+        QuestionsList[3] = "la cobaya"; //guinea pig
+        QuestionsList[4] = "el hipopótamo"; //hippo
+        QuestionsList[5] =  "el caballo"; //horse
+        QuestionsList[6] = "el ratón"; //mouse
+        QuestionsList[7] = "el cerdo"; //pig
+        QuestionsList[8] = "el tiburón"; //shark
+        QuestionsList[9] = "el tigre"; //tiger
+        QuestionsList[10] = "la tortuga"; //turtle
+        return QuestionsList;
+    }
+
+}
---
+++ app/src/main/java/com/grammar/trocket/grammar/com/grammar/trocket/audioQuiz/Audio_Quiz_Statistics_Activity.java
@@ -0,0 +1,58 @@
+package com.grammar.trocket.grammar.com.grammar.trocket.audioQuiz;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Window;
+import android.widget.TextView;
+
+import com.grammar.trocket.grammar.R;
+
+/**
+ * Created by Sam on 06/03/2016.
+ */
+public class Audio_Quiz_Statistics_Activity extends AppCompatActivity {
+
+    public TextView successCounter;
+    public TextView mistakeCounter;
+    public TextView comment;
+    public String message;
+    public String message2;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        setContentView(R.layout.activity_audio_quiz_statistics);
+
+        // Get the message from the intent
+        Intent intent = getIntent();
+        message = intent.getStringExtra(Audio_Quiz_Main_Activity.EXTRA_MESSAGE);
+        message2 = intent.getStringExtra(Audio_Quiz_Main_Activity.EXTRA_MESSAGE2);
+
+        successCounter = (TextView) findViewById(R.id.successCounter);
+        mistakeCounter = (TextView) findViewById(R.id.mistakeCounter);
+
+        comment = (TextView) findViewById(R.id.comment);
+
+        successCounter.setText("Number of successes "+ message);
+        mistakeCounter.setText("Number of mistakes "+ message2);
+
+        initializeComment();
+
+    }
+
+    /**
+     * Initializes the comment with message that changes according t0 the
+     * message received from the Quiz_Main_Activity class.
+     */
+    public void initializeComment(){
+        if(Integer.parseInt(message)>= 5){
+            comment.setText("Yatta!!");
+        }
+        else {
+            comment.setText("Try harder");
+        }
+    }
+}
