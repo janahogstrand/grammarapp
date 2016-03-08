846381cf92b627b9401040ca4c93fefa7e1e9f99
firas altayeb <firas.altayeb@kcl.ac.uk>
2016-03-08 18:20:55 +0000

    Fixed the seasons activity's XML

--- app/src/main/java/com/grammar/trocket/grammar/com/grammar/trocket/resources/seasons/SeasonsMain.java
+++ app/src/main/java/com/grammar/trocket/grammar/com/grammar/trocket/resources/seasons/SeasonsMain.java
@@ -2,13 +2,6 @@ package com.grammar.trocket.grammar.com.grammar.trocket.resources.seasons;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.design.widget.NavigationView;
-import android.support.v4.view.GravityCompat;
-import android.support.v4.widget.DrawerLayout;
-import android.support.v7.app.ActionBarDrawerToggle;
-import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -46,14 +39,6 @@ public class SeasonsMain extends BaseActivityDrawer {
 
         findAndAssignText();
 
-//        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
-//        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
-//            this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
-//        drawer.setDrawerListener(toggle);
-//        toggle.syncState();
-//
-//        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
-//        navigationView.setNavigationItemSelectedListener(this);
     }
 
     /**
@@ -72,31 +57,7 @@ public class SeasonsMain extends BaseActivityDrawer {
         autumnBtn.setText("Otono");
         winterBtn.setText("Invierno");
     }
-//
-//    @Override
-//    public void onBackPressed() {
-//        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
-//        if (drawer.isDrawerOpen(GravityCompat.START)) {
-//            drawer.closeDrawer(GravityCompat.START);
-//        } else {
-//            super.onBackPressed();
-//        }
-//
-//    }
-//
-//    @SuppressWarnings("StatementWithEmptyBody")
-//    @Override
-//    public boolean onNavigationItemSelected(MenuItem item) {
-//        // Handle navigation view item clicks here.
-//        int id = item.getItemId();
-//
-//        if (id == R.id.nav_camera) {
-//            // Handle the camera action
-//        }
-//        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
-//        drawer.closeDrawer(GravityCompat.START);
-//        return true;
-//    }
+
 
     /**
      * This method moves the user to a new activity based on the button they
