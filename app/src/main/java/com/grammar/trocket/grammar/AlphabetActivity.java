1a8747992361a6befdd8f15965a6e0db827241c2
teerachel <teerachel@hotmail.com>
2016-03-09 16:19:32 +0000

    assigned text to Buttons

--- app/src/main/java/com/grammar/trocket/grammar/AlphabetActivity.java
+++ app/src/main/java/com/grammar/trocket/grammar/AlphabetActivity.java
@@ -29,11 +29,46 @@ public class AlphabetActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_alphabet);
 
-        //Before you call this method you have to find the btns ids
-        //assignTextView();
+        /**
+         * Buttons assigned to their respective ids
+         */
+        btnA = (Button) findViewById (R.id.btnA);
+        btnB = (Button) findViewById (R.id.btnB);
+        btnChe = (Button) findViewById (R.id.btnChe);
+        btnC = (Button) findViewById (R.id.btnc);
+        btnD = (Button) findViewById (R.id.btnD);
+        btnE = (Button) findViewById (R.id.btnE);
+        btnF = (Button) findViewById (R.id.btnF);
+        btnG = (Button) findViewById (R.id.btnG);
+        btnH = (Button) findViewById (R.id.btnH);
+        btnI = (Button) findViewById (R.id.btnI);
+        btnJ = (Button) findViewById (R.id.btnJ);
+        btnK = (Button) findViewById (R.id.btnK);
+        btnL = (Button) findViewById (R.id.btnL);
+        btnLL = (Button) findViewById (R.id.btnLL);
+        btnM = (Button) findViewById (R.id.btnM);
+        btnN = (Button) findViewById (R.id.btnN);
+        btnNN = (Button) findViewById (R.id.btnn);
+        btnO = (Button) findViewById (R.id.btnO);
+        btnP = (Button) findViewById (R.id.btnP);
+        btnQ = (Button) findViewById (R.id.btnQ);
+        btnR = (Button) findViewById (R.id.btnR);
+        btnS = (Button) findViewById (R.id.btnS);
+        btnT = (Button) findViewById (R.id.btnT);
+        btnU = (Button) findViewById (R.id.btnU);
+        btnV = (Button) findViewById (R.id.btnV);
+        btnW = (Button) findViewById (R.id.btnW);
+        btnX = (Button) findViewById (R.id.btnX);
+        btnY = (Button) findViewById (R.id.btnY);
+        btnZ = (Button) findViewById (R.id.btnZ);
+        btnInfo = (Button) findViewById (R.id.btninfo);
 
+        assignTextView();
     }
 
+    /** assignTextView() is a method which sets
+     * the text of each button.
+     */
     public void assignTextView(){
 
         btnA.setText("A a");
@@ -50,23 +85,22 @@ public class AlphabetActivity extends AppCompatActivity {
         btnK.setText("K k");
         btnL.setText("L l");
         btnLL.setText("L ll");
-
         btnM.setText("M m");
         btnN.setText("N n");
         btnNN.setText("Nn o");
         btnO.setText("O o");
-        btnP.setText("btnP");
-        btnQ.setText("btnQ");
-        btnR.setText("btnR");
-        btnS.setText("btnS");
-        btnT.setText("btnT");
-        btnU.setText("btnU");
-        btnV.setText("btnV");
-        btnW.setText("btnW");
-        btnX.setText("btnX");
-        btnY.setText("btnY");
-        btnZ.setText("btnZ");
-        btnInfo.setText("btnInfo");
+        btnP.setText("P p");
+        btnQ.setText("Q q");
+        btnR.setText("R r");
+        btnS.setText("S s");
+        btnT.setText("T t");
+        btnU.setText("U u");
+        btnV.setText("V v");
+        btnW.setText("W w");
+        btnX.setText("X x");
+        btnY.setText("Y y");
+        btnZ.setText("Z z");
+        btnInfo.setText("Info");
 
     }
 
