c1043b02e1f250c1af10741721293abf769e0dea
firas altayeb <firas.altayeb@kcl.ac.uk>
2016-03-25 15:23:38 +0000

    fixed the onBackPressed for the Credits activity

--- app/src/main/java/com/grammar/trocket/grammar/com/grammar/trocket/resources/Credits.java
+++ app/src/main/java/com/grammar/trocket/grammar/com/grammar/trocket/resources/Credits.java
@@ -3,10 +3,7 @@ package com.grammar.trocket.grammar.com.grammar.trocket.resources;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.widget.TextView;
-
 import com.grammar.trocket.grammar.R;
-import com.grammar.trocket.grammar.com.grammar.trocket.exercises.TextQuizMainActivity;
 import com.grammar.trocket.grammar.com.grammar.trocket.main.MainMenu;
 
 /**
@@ -25,6 +22,5 @@ public class Credits extends AppCompatActivity{
         super.onBackPressed();
         Intent intent = new Intent(Credits.this, MainMenu.class);
         startActivity(intent);
-        this.onDestroy();
     }
 }
